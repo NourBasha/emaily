2d1a79820d58a24e43d659bfc573543928f0d864
NourB <nour.basha2011@gmail.com>
2021-01-16 18:37:30 +0200

    basic structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+
+// handler
+app.get('/', (req, res)=>{
+    res.send({hi: 'hello'});
+});
+
+
+
+
+
+// if we're in production then take the service provider port at runtime,
+// else take default for development purposes
+const PORT = process.env.PORT || 5000
+// listen to port
+app.listen(PORT);
