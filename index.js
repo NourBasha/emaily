35a908fd72e2a2c3c8681bf46a61ff1c5d7f8601
NourB <nour.basha2011@gmail.com>
2021-01-17 17:34:13 +0200

    before adding mongoD

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
+// our express instance
 const app = express();
 
 
-// handler
-app.get('/', (req, res)=>{
-    res.send({hi: 'hello again'});
-});
+
+// register strategy into passport
+passport.use(new GoogleStrategy({ 
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+ }, (accessToken, refreshToken,  profile, done) =>{
+        console.log('access token :', accessToken);
+        console.log('refresh token :', refreshToken);
+        console.log('profile:',profile);
+  })
+  );
 
 
 
+// user tries to log in with google
+app.get('/auth/google', passport.authenticate('google', {
+                 scope: ['profile','email']
+}))
 
+// user grants permission to google to use his profile and email data and redirected to our app with the CODE
+// we use the code sent back to continue and get the profile and email
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // if we're in production then take the service provider port at runtime,
 // else take default for development purposes
